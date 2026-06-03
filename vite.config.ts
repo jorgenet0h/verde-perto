5be94d07dbe34e8a5f2f9059e14e7c1bb7822ae2
jorgenet0h <jorgenet0h@gmail.com>
2026-06-03 08:41:07 -0300

    Removendo tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/start/vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    tanstackStart({
-      nitro: {
-        preset: "vercel",
-      },
-    }),
     react(),
   ],
 });
